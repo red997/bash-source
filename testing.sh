838d671a30c1bd22ef8edffee35101a6e8083e8a
root <root@my-jenkins.com>
2019-08-20 14:22:12 +0000

    commit3

--- testing.sh
+++ testing.sh
@@ -3,6 +3,3 @@ NUM1=10
 NUM2=20
 echo "Hey this is test script"
 echo "The addition of ${NUM1} and ${NUM2} is" $((${NUM1}+${NUM2}))
-echo "Hey, I am adding this content."
-echo "This is the last try "
-echo "hello bhai"
