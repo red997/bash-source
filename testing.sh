940c41d9f88ba2d1ef225d9c86c03008b58cff86
root <root@my-jenkins.com>
2019-08-23 11:29:10 +0000

    commit7

--- testing.sh
+++ testing.sh
@@ -4,6 +4,3 @@ NUM2=20
 echo "Hey this is test script"
 echo "The addition of ${NUM1} and ${NUM2} is" $((${NUM1}+${NUM2}))
 date
-echo "Hey This is updated script"
-echo "hey this commit is to test the slave-2 (Debian)"
-echo "Again ntrying to fix thr previous build"
